7e193ab22fcaa6095070c7a9eb82da0933235fdd
yash-r-gorde <yash.r.gorde@gmail.com>
2025-11-03 01:32:07 +0530

    Understand commit structure and types

---
+++ 3.2FirstCppProgram/002_comments.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+// Entry point main function
+int main(int argc, char **argv) {
+    // One line comment
+
+    /*
+        Multi-line block comment
+        Another line
+        Oh! And another one !
+    */
+
+    // Print out some text
+    std::cout << "Hello World in C++20!" << std::endl;
+    return 0;
+}
