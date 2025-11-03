ee6a69a3a4756733b63ef70f0adbee94489c6f15
yash-r-gorde <yash.r.gorde@gmail.com>
2025-11-03 23:12:25 +0530

    completed the errors and the warnings

---
+++ 3.2FirstCppProgram/003_errors_warnings.cpp
@@ -0,0 +1,14 @@
+/*
+    compile time error
+    runtime error
+    warnings
+*/
+
+#include <iostream>
+
+int main() {
+    // std::cout << "hello, world!" << std::endl  // i'm compile time error
+    // int value = 21/0; // i'm runtime error 
+    // std::cout << "value : " << value << std::endl;
+    return 0;
+}
